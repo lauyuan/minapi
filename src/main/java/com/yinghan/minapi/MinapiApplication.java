4b7775c7995644af678486dfeb2991f7172ee0ff
lauyuan <1024891772@qq.com>
2023-04-12 23:01:31 +0800

    初始化代码

---
+++ src/main/java/com/yinghan/minapi/MinapiApplication.java
@@ -0,0 +1,13 @@
+package com.yinghan.minapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yinghan/minapi/MinapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yinghan.minapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
